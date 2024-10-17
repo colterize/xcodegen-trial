ced455c46f675614bdb78eee5b6ef5ddbbc8ed2d
yaniyani <yani@ist.id>
2024-10-17 11:48:48 +0700

    setup navigation to dashboard

--- xcodegen-trial/ViewController.swift
+++ xcodegen-trial/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var showButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        showButton.addTarget(self, action: #selector(showDashboardPage), for: .touchUpInside)
     }
 
 
 }
 
+extension ViewController {
+    @objc func showDashboardPage() {
+        let vc = DashboardViewController()
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+}
