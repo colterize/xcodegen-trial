630af5eca8b6c0345f44d3a52fa60edb8b54af0d
yaniyani <yani@ist.id>
2024-10-18 10:47:44 +0700

    add file settings view

---
+++ xcodegen-trial/SettingsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.swift
+//  xcodegen-trial
+//
+//  Created by Yani . on 18/10/24.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
